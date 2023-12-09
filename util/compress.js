424fd2032f80f9777b7cc313ce42274d45691bc3
Chen, Chih-Chia <pigfoot@gmail.com>
2023-12-09 21:21:45 +0800

    [app] fork from himshim/bandwidth-hero-proxy2
    Signed-off-by: Chen, Chih-Chia <pigfoot@gmail.com>

--- util/compress.js
+++ util/compress.js
@@ -2,11 +2,18 @@
 const sharp = require("sharp");
 
 function compress(imagePath, useWebp, grayscale, quality, originalSize) {
-  let format = useWebp ? "webp" : "jpeg";
+  //let format = useWebp ? "webp" : "jpeg";
+  let format = useWebp ? "avif" : "jpeg";
+  var options = { quality };
+
+  if (format === "webp" || format === "jpeg") {
+    options["progressive"] = true;
+    options["optimizeScans"] = true;
+  }
 
   return sharp(imagePath)
     .grayscale(grayscale)
-    .toFormat(format, { quality, progressive: true, optimizeScans: true })
+    .toFormat(format, options)
     .toBuffer({ resolveWithObject: true })
     .then(({ data, info }) => ({
       err: null,
